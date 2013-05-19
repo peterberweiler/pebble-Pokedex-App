33a667b17cb6d3dedd27a065d87800356d0d96a8
shutdown27 <info@peter-berweiler.de>
2013-05-19 15:22:42 -0700

    Version 1.0

--- src/main.c
+++ src/main.c
@@ -1,169 +1,145 @@
 
-
-#include "small_functions.h"
+#include "pebble_os.h"
+#include "pebble_app.h"
+#include "pebble_fonts.h"
 	
-#define MY_UUID { 0x0A, 0x0D, 0xB9, 0x17, 0x9A, 0xFD, 0x4F, 0x5E, 0xAF, 0x0F, 0xDA, 0x89, 0x70, 0x4F, 0x64, 0x14 }
-
-
+#include "pokedex.h"
+	
+#define MY_UUID { 0xC2, 0x84, 0x93, 0x30, 0xE2, 0x28, 0x4A, 0x58, 0xA9, 0xE9, 0xAC, 0x37, 0x66, 0xD4, 0xFC, 0x8C }
 PBL_APP_INFO(MY_UUID,
-             "Pip-Boy 2013", "Peter-Berweiler.de",
+             "Pokedex App", "Peter-Berweiler.de",
              1, 0, /* App version */
              DEFAULT_MENU_ICON,
-             APP_INFO_WATCH_FACE);
+             APP_INFO_STANDARD_APP);
+
 
 
 Window window;
-TextLayer timeLayer;
-Layer line_layer;
+TextLayer textLayer;
 BmpContainer image_container;
-
-int health = 0; 
-
-
-void line_layer_update_callback(Layer *layer, GContext *ctx) {
-
-  	graphics_context_set_stroke_color(ctx, GColorWhite);
-
-  	graphics_draw_line(ctx, GPoint(0,  0), GPoint(143, 0));
-  	graphics_draw_line(ctx, GPoint(0,  1), GPoint(143, 1));
-  	graphics_draw_line(ctx, GPoint(0,  2), GPoint(143, 2));
-
-  	graphics_draw_line(ctx, GPoint(0,  3), GPoint(0, 18));
-  	graphics_draw_line(ctx, GPoint(1,  3), GPoint(1, 18));
-
-  	graphics_draw_line(ctx, GPoint(143,  3), GPoint(143, 18));
-  	graphics_draw_line(ctx, GPoint(142,  3), GPoint(142, 18));
+BmpContainer image_top;
+BmpContainer image_bottom;
+int currentID;
 
 
-  	graphics_draw_line(ctx, GPoint(2,  164), GPoint(141, 164));
-  	graphics_draw_line(ctx, GPoint(2,  165), GPoint(141, 165));
-
-  	graphics_draw_line(ctx, GPoint(2,  150), GPoint(2, 163));
-  	graphics_draw_line(ctx, GPoint(3,  150), GPoint(3, 163));
-
-	graphics_draw_line(ctx, GPoint(140,  150), GPoint(140, 163));
- 	graphics_draw_line(ctx, GPoint(141,  150), GPoint(141, 163));
+void update_selection() {
+	if (currentID < 1){    currentID = 1;   }
+	if (currentID > 152){  currentID = 152; }
 	
+	text_layer_set_text(&textLayer, poke_names[currentID-1]);
 	
+	layer_remove_from_parent(&image_container.layer.layer);
+	bmp_deinit_container(&image_container);
 	
-	//graphics_fill_rect(ctx, GRect(0, 28, 21, 4), 0, GCornerNone)
-	seed = get_seconds(); // Seed the random numbers
+	bmp_init_container(poke_images[currentID-1], &image_container);
+	layer_set_frame(&image_container.layer.layer, GRect(/*43*/ 70, 10, 58, 58));
+	layer_add_child(&window.layer, &image_container.layer.layer);
 	
-	/*** Left Arm    14 ,39***/
-	graphics_context_set_fill_color(ctx, GColorBlack);
-	graphics_fill_rect(ctx, GRect(14, 28+39+1, 21, 4), 0, GCornerNone); 
-	graphics_context_set_fill_color(ctx, GColorWhite);
-	graphics_fill_rect(ctx, GRect(14, 28+39+1, random(21), 4), 0, GCornerNone);
-	/*** Right Arm   112, 39***/
-	graphics_context_set_fill_color(ctx, GColorBlack);
-	graphics_fill_rect(ctx, GRect(112, 28+39+1, 21, 4), 0, GCornerNone); 
-	graphics_context_set_fill_color(ctx, GColorWhite);
-	graphics_fill_rect(ctx, GRect(112, 28+39+1, random(21), 4), 0, GCornerNone);
-	/*** Head        63, 6 ***/
-	graphics_context_set_fill_color(ctx, GColorBlack);
-	graphics_fill_rect(ctx, GRect(63, 28+6+1, 21, 4), 0, GCornerNone); 
-	graphics_context_set_fill_color(ctx, GColorWhite);
-	graphics_fill_rect(ctx, GRect(63, 28+6+1, random(21), 4), 0, GCornerNone);
-	/*** Body        63, 62 ***/
-	graphics_context_set_fill_color(ctx, GColorBlack);
-	graphics_fill_rect(ctx, GRect(63, 28+62+1, 21, 4), 0, GCornerNone); 
-	graphics_context_set_fill_color(ctx, GColorWhite);
-	graphics_fill_rect(ctx, GRect(63, 28+62+1, random(21), 4), 0, GCornerNone);
-	/*** Left Leg    14, 106 ***/
-	graphics_context_set_fill_color(ctx, GColorBlack);
-	graphics_fill_rect(ctx, GRect(14, 28+106+1, 21, 4), 0, GCornerNone); 
-	graphics_context_set_fill_color(ctx, GColorWhite);
-	graphics_fill_rect(ctx, GRect(14, 28+106+1, random(21), 4), 0, GCornerNone);
-	/*** Right Leg  108, 106 ***/
-	graphics_context_set_fill_color(ctx, GColorBlack);
-	graphics_fill_rect(ctx, GRect(108, 28+106+1, 21, 4), 0, GCornerNone); 
-	graphics_context_set_fill_color(ctx, GColorWhite);
-	graphics_fill_rect(ctx, GRect(108, 28+106+1, random(21), 4), 0, GCornerNone);
-		
-	/*health = health+1;
-	
-	if (health > 21){
-		health = 0;
-	}*/
 }
 
-/*** Called once per second ***************************/
-void handle_tick(AppContextRef ctx, PebbleTickEvent *t) {
-  (void)t;
-  (void)ctx;
+///////////////////////// INPUT /////////////////////
 
-  static char timeText[] = "00:00"; // Needs to be static because it's used by the system later.
+void up_single_click_handler(ClickRecognizerRef recognizer, Window *window) {
+  (void)recognizer;
+  (void)window;
+	
+	currentID = currentID -1;
+	update_selection();
+}
 
-  PblTm currentTime;
-  get_time(&currentTime);
+void down_single_click_handler(ClickRecognizerRef recognizer, Window *window) {
+  (void)recognizer;
+  (void)window;
+	
+	currentID = currentID +1;
+	update_selection();
+}
 
-  char *time_format; // "%b %d"
-  if (clock_is_24h_style()) {
-    time_format = "%H:%M"; 
-  } else {
-    time_format = "%I:%M";
-  }
+void up_long_click_handler(ClickRecognizerRef recognizer, Window *window) {
+  (void)recognizer;
+  (void)window;
+	
+	currentID = currentID -15;
+	update_selection();
+}
 
-  string_format_time(timeText, sizeof(timeText), time_format, &currentTime);
-  text_layer_set_text(&timeLayer, timeText);
+void down_long_click_handler(ClickRecognizerRef recognizer, Window *window) {
+  (void)recognizer;
+  (void)window;
+	
+	currentID = currentID +15;
+	update_selection();
+}
 
-  layer_mark_dirty( &line_layer );
+void click_config_provider(ClickConfig **config, Window *window) {
+  (void)window;
+
+	config[BUTTON_ID_UP]->click.handler      = (ClickHandler) up_single_click_handler;
+	config[BUTTON_ID_UP]->long_click.handler = (ClickHandler) up_long_click_handler;
+	
+	config[BUTTON_ID_DOWN]->click.handler      = (ClickHandler) down_single_click_handler;
+	config[BUTTON_ID_DOWN]->long_click.handler = (ClickHandler) down_long_click_handler;
 }
 
 
+///////////////////////// INIT /////////////////////
 void handle_init_app(AppContextRef app_ctx) {
 
-  window_init(&window, "Lol");
-  window_stack_push(&window, true);
-  window_set_background_color(&window, GColorBlack);
+	currentID = 1;
+		
+	window_init(&window, "Kanto Pokedex");
+	window_stack_push(&window, true);
+	window_set_background_color(&window, GColorWhite);
+	resource_init_current_app(&APP_RESOURCES);
 
-  resource_init_current_app(&FEATURE_DEMO_IMAGE_RESOURCES);
+	/*******  TEXT LAYER ********/
+	GFont custom_font = fonts_load_custom_font(resource_get_handle(RESOURCE_ID_FONT_PKMN_14));
+	text_layer_init(&textLayer, GRect(-6, 44, 150 /* width */, 98 /* height */));
+	text_layer_set_text_color(&textLayer, GColorBlack);
+	text_layer_set_overflow_mode(&textLayer, GTextOverflowModeTrailingEllipsis);
+	text_layer_set_background_color(&textLayer, GColorClear);
+	text_layer_set_text_alignment(&textLayer, GTextAlignmentCenter);
+	text_layer_set_font(&textLayer, custom_font);
 
-  seed = get_seconds(); // Seed the random numbers
+	
+	layer_add_child(&window.layer, &textLayer.layer);
+	
 
-  /*** Vault Boy Image ****************************************************/
-  bmp_init_container(RESOURCE_ID_IMAGE_VAULTBOY, &image_container);
-  layer_set_frame(&image_container.layer.layer, GRect(0, 28, 144, 140));
+	/*******  IMAGE ********/
+	bmp_init_container(RESOURCE_ID_IMAGE_poke9, &image_container);
+	                                                       // gets overwritten
+	layer_set_frame(&image_container.layer.layer, GRect(/*43*/ 0, 0, 58, 58));
+	layer_add_child(&window.layer, &image_container.layer.layer);
+	
+	/////// UI TOP ////////////
+	bmp_init_container(RESOURCE_ID_UI_TOP, &image_top);
+	layer_set_frame(&image_top.layer.layer, GRect( 0, 0, 144, 11));
+	layer_add_child(&window.layer, &image_top.layer.layer);
+	/////// UI BOTTOM ////////////
+	bmp_init_container(RESOURCE_ID_UI_BOTTOM, &image_bottom);
+	layer_set_frame(&image_bottom.layer.layer, GRect( 0, 168-11-16, 144, 11));
+	layer_add_child(&window.layer, &image_bottom.layer.layer);
 
-  /*** Line Layer */
-  layer_init(&line_layer, window.layer.frame);
-  line_layer.update_proc = &line_layer_update_callback;
-  
-  /*** Time Layer old:GRect(5, 5, 100, 28)****************************************/
-  GFont custom_font = fonts_load_custom_font(resource_get_handle(RESOURCE_ID_FONT_FALLOUT_26));
-
-  text_layer_init(&timeLayer, GRect(3, 1, 135 /* width */, 50 /* height */));
-  text_layer_set_text_color(&timeLayer, GColorWhite);
-  text_layer_set_background_color(&timeLayer, GColorClear);
-  text_layer_set_text_alignment(&timeLayer, GTextAlignmentRight);
-  text_layer_set_font(&timeLayer, custom_font);
-  
-  /*** Place Layers  *****************************************************/
-  layer_add_child(&window.layer, &image_container.layer.layer);
-  
-  layer_add_child(&window.layer, &line_layer);
-  layer_add_child(&window.layer, &timeLayer.layer);
-
-  handle_tick(app_ctx, NULL);
-
+	
+	window_set_click_config_provider(&window, (ClickConfigProvider) click_config_provider);
+	
+	update_selection();
 }
 
-/*** De Init **********************************/
+
+///////////////////////// DE INIT /////////////////////
 void handle_deinit(AppContextRef ctx) { 
   (void)ctx;
   bmp_deinit_container(&image_container);
 }
 
-/*** The main event/run loop for our app **************/
+
+///////////////////////// MAIN /////////////////////
 void pbl_main(void *params) {
   PebbleAppHandlers handlers = {
     .init_handler = &handle_init_app,
-    .deinit_handler = &handle_deinit,
-    .tick_info = {
-      .tick_handler = &handle_tick,
-      .tick_units = MINUTE_UNIT
-    }
-
+    .deinit_handler = &handle_deinit
   };
   app_event_loop(params, &handlers);
 }
+
